6b12a67206ff55f7f9c796924b1e2059e95f99bd
Juan D Chaves <juandavidchaves@protonmail.com>
2023-08-22 09:01:41 -0300

    first

---
+++ QuickConversion/QuickConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickConversionApp.swift
+//  QuickConversion
+//
+//  Created by Juan David Chaves Rodriguez on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
